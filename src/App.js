899a428d091fa54bfdbec6f4606725522ad6a2af
epicX67 <souravgain605@gmail.com>
2022-12-04 14:28:53 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/about" element={<div>About</div>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
